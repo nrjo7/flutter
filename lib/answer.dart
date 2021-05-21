d20052f9323b12fddc8b8371e4b58c24bce3adcd
nrjo7 <neerajrk007@gmail.com>
2021-05-22 04:41:08 +0530

    ElevatedButton used instead of RaisedButton

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,19 +1,19 @@
-
 import 'package:flutter/material.dart';
 
-class Answer extends StatelessWidget{
+class Answer extends StatelessWidget {
   final Function selectHandler;
-Answer(this.selectHandler);
+  Answer(this.selectHandler);
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-      width: double.infinity,
-      child: RaisedButton(
-        color: Colors.blue,
-            child: Text('Answer1'),
-            onPressed: selectHandler,
-          )
-    );
+        width: double.infinity,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.black),
+            foregroundColor: MaterialStateProperty.all(Colors.yellow)
+          ),
+          onPressed: selectHandler, 
+          child: Text('Answer 1')));
   }
 }
