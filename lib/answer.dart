dc4506b19267fb6b6234f84b1efe3191896083e1
nrjo7 <neerajrk007@gmail.com>
2021-05-23 04:04:14 +0530

    Questions and dynamic answers added, message after runnning out of question made possible with if statement

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selectHandler;
-  Answer(this.selectHandler);
+  final String answerText;
+  Answer(this.selectHandler,this.answerText);
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -10,10 +11,10 @@ class Answer extends StatelessWidget {
         width: double.infinity,
         child: ElevatedButton(
           style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(Colors.black),
-            foregroundColor: MaterialStateProperty.all(Colors.yellow)
+            backgroundColor: MaterialStateProperty.all(Colors.blue[900]),
+            foregroundColor: MaterialStateProperty.all(Colors.white)
           ),
           onPressed: selectHandler, 
-          child: Text('Answer 1')));
+          child: Text(answerText)));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,61 @@
 import 'package:flutter/material.dart';
 import './answer.dart';
 import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-@override
-State<StatefulWidget> createState(){
- return _MyAppState();
-}
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
 
-class _MyAppState extends State<MyApp>{
-  int _questionIndex=0;
+class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favourite colour?',
+      'answer': ['Black', 'Pink', 'Blue', 'Red']
+    },
+    {
+      'questionText': 'Which\'s the worst year of your life?',
+      'answer': ['1999', '2021', '2020', '2017']
+    },
+  ];
+  int _questionIndex = 0;
   void _answerQuestion() {
-    setState((){
-      _questionIndex ++;
+    setState(() {
+      _questionIndex++;
     });
-  
+
     print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print("You got another question");
+    } else {
+      print('No more questions');
+    }
   }
 
   @override
   build(BuildContext context) {
-    var questions = [
-      'What\'s your favourite colour?',
-      'Who\'s your favourite person'
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(children: [
-          Question(questions[_questionIndex]),
-          Answer(_answerQuestion),
-          Answer(_answerQuestion),
-          Answer(_answerQuestion),
-          
-        ]),
+        body: _questionIndex < questions.length
+            ? Column(children: [
+                Question(questions[_questionIndex]['questionText']),
+                ...(questions[_questionIndex]['answer'] as List<String>)
+                    .map((answer) {
+                  return Answer(_answerQuestion, answer);
+                }).toList()
+              ])
+            : Center(
+                child: Text('You did it'),
+              ),
       ),
     );
   }
